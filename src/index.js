67c297d5826cbd5427dc9fc20ccb62aa8ae44a7f
yusei <yusei5884@gmail.com>
2019-08-14 14:20:17 +0900

    storeの配置を元に戻す

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/App'
 import { Provider } from 'react-redux'
-import { store } from './store'
 import './styles/index.css'
+import chatReducer from './reducers/chats'
+import logger from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
+
+const store = createStore(
+    chatReducer,
+    applyMiddleware(logger)
+)
 
 ReactDOM.render(
         <Provider store={store}>
